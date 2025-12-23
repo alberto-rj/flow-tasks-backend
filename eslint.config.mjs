e29612493911fa728c466df17dbc33e378d11ded
Alberto José <albertorauljose2@gmail.com>
2025-12-23 06:31:06 +0100

    style: add eslint and prettier configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.ts'],
+
+    languageOptions: {
+      parser: parser,
+      sourceType: 'module',
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: ['build', 'node_modules'],
+  },
+];
