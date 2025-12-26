045a4ff17ceeed6041eb10c6f28f3f3af8a9d139
Alberto José <albertorauljose2@gmail.com>
2025-12-26 02:48:13 +0100

    refactor(app): move server startup logic to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,37 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import { StatusCodes } from 'http-status-codes';
+
+import { load } from '@/config/env';
+
+const { PORT, NODE_ENV } = load();
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (_, res) => {
-  res.status(200).json({
+  res.status(StatusCodes.OK).json({
     success: true,
     message: 'Hello world',
   });
 });
 
 app.get('/api/health', (_, res) => {
-  res.status(200).json({
+  res.status(StatusCodes.OK).json({
     success: true,
+    mode: NODE_ENV,
     timestamp: new Date().toISOString(),
     uptime: process.uptime(),
   });
 });
+
+if (NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(
+      `Server is running at http://localhost:${PORT} in "${NODE_ENV}" mode.`,
+    );
+  });
+}
