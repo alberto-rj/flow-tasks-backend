ae9897d4e22390db395e8e021148ab446dcc6055
Alberto José <albertorauljose2@gmail.com>
2026-01-10 07:26:36 +0100

    test(todo): add integration tests for todo deletion endpoint
    - add test setup and teardown
    - cover the flowing cases:
      - successful todo deletion
      - todo ID validation error
      - unauthenticated access
      - non-existing todo

---
+++ src/routes/todos/todos.delete.route.spec.ts
@@ -0,0 +1,80 @@
+import { StatusCodes } from 'http-status-codes';
+import supertest from 'supertest';
+import { describe, it, afterEach, beforeEach } from 'vitest';
+
+import { app } from '@/app';
+import type { TodoDto } from '@/dtos/todo';
+import {
+  cleanup,
+  expectError,
+  expectValidationError,
+  getAuthenticatedAgent,
+  getCreatedTodo,
+  TODOS_BASE_ROUTE,
+  type SuperTestAgent,
+} from '@/utils/test.route';
+import { uuid } from '@/utils/uuid';
+
+describe(`DELETE ${TODOS_BASE_ROUTE}/:todoId`, () => {
+  let agent: SuperTestAgent;
+  let createdTodo: TodoDto;
+
+  beforeEach(async () => {
+    agent = await getAuthenticatedAgent();
+    createdTodo = await getCreatedTodo(agent);
+  });
+
+  afterEach(async () => {
+    await cleanup();
+  });
+
+  describe('success cases', () => {
+    it('should return 204 and remove the todo resource', async () => {
+      await agent
+        .delete(`${TODOS_BASE_ROUTE}/${createdTodo.todoId}`)
+        .expect(StatusCodes.NO_CONTENT);
+
+      const response = await agent
+        .get(`${TODOS_BASE_ROUTE}/${createdTodo.todoId}`)
+        .expect(StatusCodes.NOT_FOUND);
+
+      expectError(response);
+    });
+  });
+
+  describe('validation errors', () => {
+    describe('params validation', () => {
+      describe('todoId', () => {
+        it('should return 422 when todoId is not a valid UUID', async () => {
+          const invalidTodoId = 'invalid-todo-id';
+
+          const response = await agent
+            .delete(`${TODOS_BASE_ROUTE}/${invalidTodoId}`)
+            .expect(StatusCodes.UNPROCESSABLE_ENTITY);
+
+          expectValidationError(response, 'todoId');
+        });
+      });
+    });
+  });
+
+  describe('business logic errors', () => {
+    it('should return 401 when user is not authenticated', async () => {
+      const response = await supertest(app)
+        .delete(`${TODOS_BASE_ROUTE}/${createdTodo.todoId}`)
+        .expect(StatusCodes.UNAUTHORIZED);
+
+      expectError(response);
+    });
+
+    it('should return 404 when todo does not exist', async () => {
+      const nonExistingTodoId = uuid();
+
+      const response = await agent
+        .delete(`${TODOS_BASE_ROUTE}/${nonExistingTodoId}`)
+        .expect(StatusCodes.NOT_FOUND);
+
+      expectError(response);
+    });
+  });
+});
